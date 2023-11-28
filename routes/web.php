151296905954f1874c3051878c4a284f686a1f22
fares <fares@stargrid.pro>
2023-11-27 23:41:32 -0300

    feat: todas as navegações do MVP criadas]

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use App\Http\Controllers\AgenciaController;
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\ContaController;
+use App\Http\Controllers\DependenteController;
+use App\Http\Controllers\EnderecosController;
 use App\Http\Controllers\FuncionarioController;
+use App\Http\Controllers\TransacoesController;
 use App\Http\Controllers\UsuarioController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,13 +23,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'nullbank.login')->name('home');
 
-Route::resource('/users', UsuarioController::class)->names('customers');
-Route::post('/users/login', [ UsuarioController::class, 'login' ])->name('users.login');
-
-Route::get('/register', [ UsuarioController::class, 'register' ])->name('customers.register');
+//Route::resource('/users', UsuarioController::class)->names('users');
+//Route::post('/users/login', [ UsuarioController::class, 'login' ])->name('users.login');
+//
+//Route::get('/register', [ UsuarioController::class, 'register' ])->name('customers.register');
 
 Route::resource('/agencies', AgenciaController::class)->names('agencies');
 Route::resource('/employees', FuncionarioController::class)->names('employees');
+Route::resource('/customers', ClienteController::class)->names('customers');
+Route::resource('/accounts', ContaController::class)->names('accounts');
+Route::resource('/dependants', DependenteController::class)->names('dependants');
+Route::resource('/addresses', EnderecosController::class)->names('addresses');
+Route::resource('/transactions', TransacoesController::class)->names('transactions');
 
 //Route::middleware('guest')->group(function () {
 //    Route::get('login', Login::class)
